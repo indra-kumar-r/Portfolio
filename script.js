73580b62eefed7bd79915db844d864dfac5f4e5c
INDRA KUMAR R <indra@geektrust.in>
2025-11-27 22:58:02 +0530

    refactor: replace video backgrounds with image overlay and styles file

---
+++ script.js
@@ -0,0 +1,13 @@
+function openResume() {
+    window.open(
+        'https://drive.google.com/file/d/1BfHWFxOJuUw6GthluoX-lRG7v5NzcAtE/view?usp=drive_link',
+        '_blank'
+    );
+}
+
+function openVideoResume() {
+    window.open(
+        'https://drive.google.com/file/d/1E5UUTtHS6AM0xForCMQbS4pLpd85MhTM/view?usp=sharing',
+        '_blank'
+    );
+}
