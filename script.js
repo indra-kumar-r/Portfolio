19dd9d529eb0799e1255fca62e80270bd7a7990d
INDRA KUMAR R <indra@geektrust.in>
2026-01-18 15:04:15 +0530

    feat: redirect to new portfolio

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function openResume() {
-    window.open(
-        'https://drive.google.com/file/d/1BfHWFxOJuUw6GthluoX-lRG7v5NzcAtE/view?usp=drive_link',
-        '_blank'
-    );
-}
+let seconds = 3;
+const countdown = document.getElementById('countdown');
 
-function openVideoResume() {
-    window.open(
-        'https://drive.google.com/file/d/1E5UUTtHS6AM0xForCMQbS4pLpd85MhTM/view?usp=sharing',
-        '_blank'
-    );
-}
+const timer = setInterval(() => {
+    seconds--;
+    countdown.textContent = seconds;
+
+    if (seconds <= 0) {
+        clearInterval(timer);
+        window.location.href = 'https://indra-kumar-r-v1.vercel.app/';
+    }
+}, 1000);
